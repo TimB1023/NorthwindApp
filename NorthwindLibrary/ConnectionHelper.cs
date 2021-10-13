626ae6c2cb72f9f6383c11ae9b9c4843c857c06f
TimB1023 <39093958+TimB1023@users.noreply.github.com>
2021-10-13 18:55:01 +0200

    Add project files.

---
+++ NorthwindLibrary/ConnectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindLibrary
+{
+    public static class ConnectionHelper
+    {
+        public static string CnnVal(string name)
+        {
+            // Gets connection string from the App.config file (needs to be added - see OneNote on this project)
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
---
+++ NorthwindLibrary/NorthwindMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindLibrary
+{
+    public class NorthwindMethods
+    {
+        public static void MyMethod()
+        {
+
+            int a = 5;
+            a += 1;
+
+        }
+
+    }
+}
